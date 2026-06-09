092330ec344fb97e26fe0c5c8f6aa37f5debaeaf
Dhruv <dhruvsapaliga12@gmail.com>
2026-06-09 18:01:25 +0530

    Completed execersie 1.8

--- Part 1/unicafe/src/App.jsx
+++ Part 1/unicafe/src/App.jsx
@@ -1,5 +1,5 @@
 import {useState} from "react"
-const statistics=(props)=>{
+const Statistics=(props)=>{
   let total = props.good+props.neutral+props.bad
   let average=(props.good-props.bad)/total
   let positive=(props.good/total)*100
@@ -10,28 +10,26 @@ const statistics=(props)=>{
       <p>neutral {props.neutral}</p>
       <p>bad {props.bad}</p>
       <p>all {total}</p>
-      <p>average{average}</p>
-      <p>positive{positive} %</p>
+      <p>average {average}</p>
+      <p>positive {positive} %</p>
     </div>
   )
 }
-const app=()=>{
+const App=()=>{
   const [good,setGood]=useState(0)
   const [neutral,setNeutral]=useState(0)
   const [bad,setBad]=useState(0)
   const handleGood=()=>{setGood(good+1)}
   const handleNeutral=()=>{setNeutral(neutral+1)}
   const handleBad=()=>{setBad(bad+1)}
-  let total = good+neutral+bad
-  let average=(good-bad)/total
-  let positive=(good/total)*100
   return(
     <div>
       <h1>give feedback</h1>
       <button onClick={handleGood}>good</button>
       <button onClick={handleNeutral}>neutral</button>
       <button onClick={handleBad}>bad</button>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
-export default app
+export default App
