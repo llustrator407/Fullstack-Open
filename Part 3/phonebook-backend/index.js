9dcfcc2c15a2fa7c67338008e1b1f084aa30ba3b
Dhruv <dhruvsapaliga12@gmail.com>
2026-06-12 20:25:45 +0530

    Completed 3.6

--- Part 3/phonebook-backend/index.js
+++ Part 3/phonebook-backend/index.js
@@ -67,6 +67,23 @@ app.delete('/api/persons/:id',(request,response)=>{
 })
 app.post('/api/persons',(request,response)=>{
   const body=request.body
+  if(!body.name || !body.number){
+    return response.status(400).json({
+      error:'name or number missing'
+    })
+  }
+  let nameExists=false
+  for(let i=0; i<persons.length; i++){
+    if(persons[i].name.toLowerCase()===body.name.toLowerCase()){
+      nameExists=true
+      break
+    }
+  }
+  if(nameExists){
+    return response.status(400).json({
+      error:'name must be unique'
+    })
+  }
   const randomId=Math.floor(Math.random()*10000000)
   const person={
     id:String(randomId),
