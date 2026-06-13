d29385c87ed96f99e7b2fe3fa03412b889bedc77
Dhruv <dhruvsapaliga12@gmail.com>
2026-06-13 14:07:47 +0530

    Completed 3.8

--- Part 3/phonebook-backend/index.js
+++ Part 3/phonebook-backend/index.js
@@ -1,7 +1,13 @@
 const express=require('express')
 const morgan=require('morgan')
 const app=express()
-app.use(morgan('tiny'))
+morgan.token('post-body',(request)=>{
+  if(request.method==='POST'){
+    return JSON.stringify(request.body)
+  }
+  return ''
+})
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post-body'))
 let persons=[
   {
     id: "1",
