891065a11ef52dae54f7c9e01b0516a94d9fda1c
Dhruv <dhruvsapaliga12@gmail.com>
2026-06-15 14:50:31 +0530

    Completed 3.12

--- Part 3/phonebook-backend/mongo.js
+++ Part 3/phonebook-backend/mongo.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+require('dotenv').config()
 if (process.argv.length < 3) {
   console.log('give password as argument')
   process.exit(1)
@@ -6,7 +7,7 @@ if (process.argv.length < 3) {
 const password = process.argv[2]
 const name = process.argv[3]
 const number = process.argv[4]
-const url = ``
+const url = process.env.MONGODB_URI
 mongoose.set('strictQuery', false)
 mongoose.connect(url)
 const personSchema = new mongoose.Schema({
