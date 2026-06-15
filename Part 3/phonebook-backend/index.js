ac81c993c8c8b1acb2925cd461680d5c7a2d2d41
Dhruv <dhruvsapaliga12@gmail.com>
2026-06-15 17:34:54 +0530

    Completed 3.14

--- Part 3/phonebook-backend/index.js
+++ Part 3/phonebook-backend/index.js
@@ -2,9 +2,12 @@ require('dotenv').config()
 const express=require('express')
 const morgan=require('morgan')
 const Person = require('./models/person')
+const path = require('path')
 const cors = require('cors')
 const app=express()
 app.use(cors())
+app.use(express.json())
+app.use(express.static('dist'))
 morgan.token('post-body',(request)=>{
   if(request.method==='POST'){
     return JSON.stringify(request.body)
@@ -12,73 +15,29 @@ morgan.token('post-body',(request)=>{
   return ''
 })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post-body'))
-let persons=[
-  {
-    id: "1",
-    name: "Arto Hellas",
-    number: "040-123456"
-  },
-  {
-    id: "2",
-    name: "Ada Lovelace",
-    number: "39-44-5323523"
-  },
-  {
-    id: "3",
-    name: "Dan Abramov",
-    number: "12-43-234345"
-  },
-  {
-    id: "4",
-    name: "Mary Poppendieck",
-    number: "39-23-6423122"
-  },
-  {
-    id: "5",
-    name: "Dhruv",
-    number: "39-23-6423122"
-  }
-]
-app.use(express.json())
-app.use(express.static('dist'))
 app.get('/api/persons',(request,response)=>{
   Person.find({}).then(persons => {
     response.json(persons)
   })  
 })
 app.get('/info',(request,response)=>{
-  const entries=persons.length
-  const date=new Date()
-  response.send(`
-    <p>Phonebook has info for ${entries} people</p>
-    <p>${date}</p>
-  `)
+  Person.countDocuments({})
+    .then(count => {
+      response.send(`<p>Phonebook has info for ${count} people</p><p>${new Date()}</p>`)
+    })
+    .catch(error => next(error))
 })
 app.get('/api/persons/:id',(request,response)=>{
-  const id=request.params.id
-  let person=null
-  for(let i=0; i<persons.length; i++){
-    if(persons[i].id===id){
-      person=persons[i]
-      break
-    }
-  }
-  if(person){
-    response.json(person)
-  }else{
-    response.status(404).end()
-  }
+  Person.findById(request.params.id)
+    .then(person => {
+      person ? response.json(person) : response.status(404).end()
+    })
+    .catch(error => next(error))
 })
 app.delete('/api/persons/:id',(request,response)=>{
-  const id=request.params.id
-  let remainingPersons=[]
-  for(let i=0; i<persons.length; i++){
-    if(persons[i].id!==id){
-      remainingPersons.push(persons[i])
-    }
-  }
-  persons=remainingPersons
-  response.status(204).end()
+  Person.findByIdAndDelete(request.params.id)
+    .then(() => response.status(204).end())
+    .catch(error => next(error))
 })
 app.post('/api/persons',(request,response)=>{
   const body=request.body
@@ -87,26 +46,13 @@ app.post('/api/persons',(request,response)=>{
       error:'name or number missing'
     })
   }
-  let nameExists=false
-  for(let i=0; i<persons.length; i++){
-    if(persons[i].name.toLowerCase()===body.name.toLowerCase()){
-      nameExists=true
-      break
-    }
-  }
-  if(nameExists){
-    return response.status(400).json({
-      error:'name must be unique'
-    })
-  }
-  const randomId=Math.floor(Math.random()*10000000)
-  const person={
-    id:String(randomId),
-    name:body.name,
-    number:body.number
-  }
-  persons.push(person)
-  response.json(person)
+  const person=new Person({
+    name: body.name,
+    number: body.number,
+  })
+  person.save()
+    .then(savedPerson => response.json(savedPerson))
+    .catch(error => next(error))
 })
 app.get((req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'))
